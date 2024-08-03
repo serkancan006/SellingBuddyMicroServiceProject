5930a29cfdd8b7c9368b3ffe57eb0baef5d689af
serkancan <serkancan@DESKTOP-4KCP4EG>
2024-08-03 19:50:58 +0300

    B3 EventBus Base

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/EventBusConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base
+{
+    public class EventBusConfig
+    {
+        public int ConnectionRetryCount { get; set; } = 5; // en fazla 5 kere dene bağlanmayı
+
+        public string DefaultTopicName { get; set; } = "SellingBuddyEventBus"; // Exchange ve topic dıiardan verilmez ise default value
+
+        public string EventBusConnectionString { get; set; } = String.Empty;  
+
+        public string SubscriberClientAppName { get; set; } = String.Empty;  // hangi servis queue yaratacak notificationapp, orderapp => OrderService.OrderCreated
+
+        public string EventNamePrefix { get; set; } = String.Empty;  // OrderCreatedIntegrationEvent yerine OrderCreated yapamsı için
+
+        public string EventNameSuffix { get; set; } = "IntegrationEvent";
+
+        public EventBusType EventBusType { get; set; } = EventBusType.RabbitMQ;
+
+        public object Connection { get; set; }
+
+
+        public bool DeleteEventPrefix => !String.IsNullOrEmpty(EventNamePrefix);
+        public bool DeleteEventSuffix => !String.IsNullOrEmpty(EventNameSuffix);
+    }
+
+    public enum EventBusType
+    {
+        RabbitMQ = 0,
+        AzureServiceBus = 1
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+        [JsonProperty]
+        public Guid Id { get; private set; }
+
+        [JsonProperty]
+        public DateTime CreatedDate { get; private set; }
+
+
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid ıd, DateTime createdDate)
+        {
+            Id = ıd;
+            CreatedDate = createdDate;
+        }
+    }
+}
+// Jsondan set edildiği zaman dışardan gelen serilization ve desirlezition id ve createddate parametlerini buradan alıp set edildiği zaman dışardan gelen id ve created değeri alıp set edelim diye yaptık
